9af3e63ff9709f26ccabfbaead436ad6a8d6ebea
Arjun1303 <138849440+Arjun1303@users.noreply.github.com>
2024-05-15 16:10:22 +0530

    Add files via upload

---
+++ Emotion Detection/main.py
@@ -0,0 +1,9 @@
+import numpy as np
+print(np.__version__)
+import matplotlib as plt
+print(plt.__version__)
+import pandas as pd
+print(pd.__version__)
+import tkinter as tk
+print(tk.TkVersion)
+# print(tk.__version__)
